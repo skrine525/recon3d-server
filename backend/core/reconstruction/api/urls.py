305e124c99125194e4b52907b25470fcc1b37f82
Skrine <tsaregorodtsev03@mail.ru>
2025-06-26 03:30:14 +1000

    [WIP]

--- backend/core/reconstruction/api/urls.py
+++ backend/core/reconstruction/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CalculateInitialMaskView, CalculateHoughView
+from .views import CalculateInitialMaskView, CalculateHoughView, CalculateMeshView
 
 urlpatterns = [
     path('calculate-initial-mask', CalculateInitialMaskView.as_view(), name='calculate-initial-mask'),
     path('calculate-hough', CalculateHoughView.as_view(), name='calculate-hough'),
+    path('calculate-mesh', CalculateMeshView.as_view(), name='calculate-mesh'),
 ] 
