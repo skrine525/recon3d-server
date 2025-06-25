95eabc1ea1c747b452a78e0e838bc008ebdc5585
Skrine <tsaregorodtsev03@mail.ru>
2025-06-25 13:25:42 +1000

    [WIP]

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -215,7 +215,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'django-static/'
 # Директория, куда `collectstatic` будет собирать все статические файлы для продакшена.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
