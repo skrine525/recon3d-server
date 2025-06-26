2f7e03bd3734742194ec4e1529bf74b3737f24f4
Skrine <tsaregorodtsev03@mail.ru>
2025-06-27 00:20:17 +1000

    [WIP]

--- backend/core/mesh_render/views.py
+++ backend/core/mesh_render/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from reconstruction.models import Reconstruction
+import json
 
 # Create your views here.
 
 def mesh_view(request, id):
     reconstruction = get_object_or_404(Reconstruction, id=id)
     mesh_url = f"/media/{reconstruction.mesh_file_path}"
-    return render(request, 'mesh_render/mesh_view.html', {'id': id, 'mesh_url': mesh_url})
+    
+    # Считываем координаты из query-параметров
+    try:
+        x = float(request.GET.get('x'))
+        y = float(request.GET.get('y'))
+        z = float(request.GET.get('z'))
+        target_point = {'x': x, 'y': y, 'z': z}
+    except (TypeError, ValueError):
+        target_point = None
+
+    context = {
+        'id': id,
+        'mesh_url': mesh_url,
+        'target_point_json': json.dumps(target_point)
+    }
+    return render(request, 'mesh_render/mesh_view.html', context)
