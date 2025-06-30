6de8831df8393ed91a36bd3142919b94a9250d2c
Skrine <tsaregorodtsev03@mail.ru>
2025-07-01 00:27:27 +1000

    [WIP]

--- backend/docker/app/run.sh
+++ backend/docker/app/run.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+set -e
+
 if [ "$1" = "-d" ] || [ "$1" = "--dev" ]; then
     poetry run python manage.py collectstatic --noinput
-    poetry run python manage.py runserver 0.0.0.0:80
+    exec poetry run python manage.py runserver 0.0.0.0:80
+
+elif [ "$1" = "celery" ]; then
+    while ! < /dev/tcp/$DATABASE_HOST/$DATABASE_PORT; do sleep 1; done;
+
+    WORKERS_COUNT=${CELERY_WORKERS:-1}
+    exec poetry run celery -A core worker -l info --concurrency=${WORKERS_COUNT}
+
 else
     while ! < /dev/tcp/$DATABASE_HOST/$DATABASE_PORT; do sleep 1; done;
     poetry run python manage.py migrate
-    poetry run gunicorn core.wsgi -b 0.0.0.0:80 --workers=5 --timeout 60
+    exec poetry run gunicorn core.wsgi -b 0.0.0.0:80 --workers=${GUNICORN_WORKERS} --timeout 60
 fi
